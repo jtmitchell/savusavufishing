267368cbcdb14ae056c983824e4719f158e40870
James Mitchell <say.wibble@gmail.com>
2016-04-04 20:31:44 +1200

    Add the ngmap directive to replace the iframe map and the photo

--- app/scripts/entry.js
+++ app/scripts/entry.js
@@ -3,3 +3,8 @@ global.jQuery = require('jquery');
 require('jquery.googleslides');
 // require('feedwidget');
 require('wsk.main');
+
+var angular = require('angular');
+var ngmap = require('ngmap');
+
+angular.module('SavusavuFishingApp', [ngmap]);
